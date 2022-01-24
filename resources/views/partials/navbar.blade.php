6cf35b67ecf97bb98697d1109c31e6646ea0091e
LailaFiqyRahayu <3103120126@student.smktelkom-pwt.sch.id>
2022-01-24 21:14:27 +0700

    update modul 2

---
+++ resources/views/partials/navbar.blade.php
@@ -0,0 +1,15 @@
+<nav class="navbar navbar-expand-lg navbar-dark bg-danger">
+  <div class="container">
+    <a class="navbar-brand" href="/">Laila</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+      <div class="navbar-nav">
+        <a class="nav-link {{ ( $title === 'Home') ? 'active' : '' }}" aria-current="page" href="/">Home</a>
+        <a class="nav-link" {{ ( $title === 'About') ? 'active' : '' }} href="/about">About</a>
+        <a class="nav-link" {{ ( $title === 'Gallery') ? 'active' : '' }} href="/gallery">Gallery</a>
+      </div>
+    </div>
+  </div>
+</nav>
--- routes/web.php
+++ routes/web.php
@@ -2,23 +2,15 @@
 
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
-
 Route::get('/', function () {
-    return view('home');
+    return view('home', [
+        "title" => "Home"
+    ]);
 });
 
 Route::get('/about', function () {
     return view('about', [
+        "title" => "About",
         "nama" => "Laila Fiqy Rahayu",
         "email" => "3103120126@student.smktelkom-pwt.sch.id",
         "gambar" => "Laila.jpg"
@@ -26,5 +18,7 @@ Route::get('/about', function () {
 });
 
 Route::get('/gallery', function () {
-    return view('gallery');
+    return view('gallery', [
+        "title" => "Gallery"
+    ]);
 });
